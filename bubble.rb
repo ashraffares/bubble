3ca8176ece0555199c7a32e8c236150f8287ae0e
ashraffares <ashraffares090@gmail.com>
2021-02-09 17:28:13 +0200

    fixing indentation line 4

--- bubble.rb
+++ bubble.rb
@@ -1,7 +1,7 @@
 ar = [15, 16, 6, 8, 5]
 len = ar.length
 (0...len - 1).each do |i|
-   (0...(len - 1) - i).each do |j|
+  (0...(len - 1) - i).each do |j|
     if ar[j] > ar[j + 1]
       ar[j], ar[j + 1] = ar[j + 1], ar[j]
     end
