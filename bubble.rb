8d11621b29213cf2d22802a0440cfb9b3c2d5a85
Mandeln <malistgulvexohmlosur@gmail.com>
2021-02-09 17:08:33 +0100

    add fix linter

--- bubble.rb
+++ bubble.rb
@@ -10,7 +10,8 @@ end
 # Second version
 def bubble_sort(array)
     loop do
-      sorted = false(array.length - 1).times do |i |
+      sorted = false
+        (array.length - 1).times do |i |
         if array[i] > array[i 1]
     array[i], array[i 1] = array[i 1], array[i]
     sorted = true
