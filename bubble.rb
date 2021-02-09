9562e76f19f55c3f530a18df9e7f2c0f1431849e
Mandeln <malistgulvexohmlosur@gmail.com>
2021-02-09 16:06:47 +0100

    fix linter

--- bubble.rb
+++ bubble.rb
@@ -1,12 +1,12 @@
-ar = Array.new
+
 ar = [15, 16, 6, 8, 5]
 len = ar.length
 for i in 0...len - 1
-    for j in 0...(len - 1) - i
-        if ar[j] > ar[j + 1]
-           ar[j] , ar[j + 1] = ar[j + 1] , ar[j]
-        end
+  for j in 0...(len - 1) - i
+    if ar[j] > ar[j + 1]
+      ar[j], ar[j + 1] = ar[j + 1], ar[j]
     end
+  end
 end
 # Second version
 def bubble_sort(array)
@@ -20,8 +20,7 @@ def bubble_sort(array)
       end
       break unless sorted
     end
-    array
-  end
+  array
+end
   unsorted_array = [5, 2, 100, 101, 6, 1]
   p bubble_sort(unsorted_array)
-  
