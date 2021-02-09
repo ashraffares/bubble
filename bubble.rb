6b145f3d17430be2bacf84fd41db9803d11946d9
ashraffares <ashraffares090@gmail.com>
2021-02-09 17:56:03 +0200

    fixing linter

--- bubble.rb
+++ bubble.rb
@@ -7,3 +7,20 @@ len = ar.length
     end   
   end
 end
+# Second version
+def bubble_sort(array)
+  loop do
+    sorted = false
+      (array.length - 1).times do |i|
+        if array[i] > array[i + 1]
+          array[i], array[i + 1] = array[i + 1], array[i]
+          sorted = true
+        end
+      end
+      break unless sorted
+  end
+  array
+end
+
+unsorted_array = [5, 2, 100, 101, 6, 1]
+p bubble_sort(unsorted_array)
