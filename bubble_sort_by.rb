479bf69eb5186de9889eb898dc9b85e220bdc883
Mandeln <malistgulvexohmlosur@gmail.com>
2021-02-10 11:17:16 +0100

    add alternative

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -9,4 +9,28 @@ def bubble_sort_by(arr)
   arr
 end
 
-print bubble_sort_by([15, 16, 6, 8, 5]) { |a, b| a > b }
+print bubble_sort_by(%w[hey hello hi howdy]) { |a, b| a > b }
+
+bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
+end
+
+# Alternative version
+
+def bubble_sort_by1(array)
+  sorted = true
+  loop do
+    sorted = false
+    (array.length - 1).times do |i|
+      diff = yield(array[i], array[i + 1]).to_i
+      if diff >= 0
+        array[i], array[i + 1] = array[i + 1], array[i]
+        sorted = true
+      end
+    end
+    break unless sorted
+  end
+  array
+end
+
+print bubble_sort_by1(%w[hey hello hi howdy])
