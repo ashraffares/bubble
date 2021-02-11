eb891e22a34466fcb2d72da95052222f30b9a309
Mandeln <malistgulvexohmlosur@gmail.com>
2021-02-11 11:03:04 +0100

    add fix bubble.rb

--- bubble.rb
+++ bubble.rb
@@ -1,17 +1,11 @@
-ar = [15, 16, 6, 8, 5]
-len = ar.length
-(0...len - 1).each do |i|
-  (0...(len - 1) - i).each do |j|
-    ar[j], ar[j + 1] = ar[j + 1], ar[j] if ar[j] > ar[j + 1]
-  end
-end
-# Second version
+# First task
+
 def bubble_sort(array)
   loop do
     sorted = false
     (array.length - 1).times do |i|
-      if array[i] > array[i 1]
-        array[i], array[i 1] = array[i 1], array[i]
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
         sorted = true
       end
     end
@@ -21,31 +15,18 @@ def bubble_sort(array)
 end
 unsorted_array = [5, 2, 100, 101, 6, 1]
 p bubble_sort(unsorted_array)
+
 # Second task
-def bubble_sort_by(arr)
-  len = arr.length
-  (0...len - 1).each do |i|
-    (0...(len - 1) - i).each do |j|
-      res = yield(arr[j], arr[j + 1])
-      arr[j], arr[j + 1] = arr[j + 1], arr[j] if res
-    end
-  end
-  arr
-end
 
-print bubble_sort_by(%w[hey hello hi howdy]) { |a, b| a > b }
-
-bubble_sort_by(%w[hi hello hey]) do |left, right|
-  left.length - right.length
-end
-# Alternative version
 def bubble_sort_by1(array)
   sorted = true
   loop do
     sorted = false
-    (array.length - 1).times do |i|
+    array.length.times do |i|
+      next unless array[i + 1]
+
       diff = yield(array[i], array[i + 1]).to_i
-      if diff >= 0
+      if diff.positive?
         array[i], array[i + 1] = array[i + 1], array[i]
         sorted = true
       end
@@ -55,4 +36,8 @@ def bubble_sort_by1(array)
   array
 end
 
-print bubble_sort_by1(%w[hey hello hi howdy])
+unsorted = %w[howdoyoudo greetings hey hello hi]
+
+p bubble_sort_by1(unsorted) { |left, right|
+  left.length <=> right.length
+}
